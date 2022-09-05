4515115dae307873b0e39aa2e86edce601e9e7a1
Pankaj Hodage <pankaj.mj22@gmail.com>
2022-09-05 15:33:49 +0530

    Address entity added

---
+++ backend/Food_Delivery_Server/src/main/java/com/app/entities/Address.java
@@ -0,0 +1,29 @@
+package com.app.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "address")
+@Getter
+@Setter
+@ToString
+public class Address extends BaseEntity
+{
+	@Column(name = "add_line_1" ,length =50)
+	String line1;
+	@Column(name = "add_line_2",length =50)
+	String line2;
+	@Column(name = "contact_no",length =15)
+	String contactNo;
+	int pincode;
+	String city;
+	String State;
+	
+	
+}
