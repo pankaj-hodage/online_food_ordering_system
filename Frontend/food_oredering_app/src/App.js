a285ff85df16ee42b4fce20c90b7a8683fa2e22b
Pankaj Hodage <pankaj.mj22@gmail.com>
2022-09-13 15:54:17 +0530

    Add address component added

--- Frontend/food_oredering_app/src/App.js
+++ Frontend/food_oredering_app/src/App.js
@@ -1,12 +1,23 @@
+import { BrowserRouter, Route, Routes, Link } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
-import Home from './Pages/homePage/Home';
+import AddAddress from './Pages/Customer/AddAddress';
 
 
 function App() {
   return (
     <div>
+    <BrowserRouter>
 
-      <Home/>
+    <Routes>
+        <Route exact path='/AddAddress' element={<AddAddress/>}/>
+    </Routes>
+
+    {/* this container is used to show toast messages */}
+    <ToastContainer position='top-center' autoClose={1000} />
+
+    </BrowserRouter>
     </div>
   );
 }
